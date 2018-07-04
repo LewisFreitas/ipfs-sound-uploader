6930e8a5a3fb6135529828dfccd9c2f448ebd2e4
luisfreitas <>
2018-07-04 17:52:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -101,10 +101,10 @@ class App extends Component {
         <main className="container">
           <div className="pure-g">
             <div className="pure-u-1-1">
-              <h1>Your Image</h1>
-              <p>This image is stored on IPFS & The Ethereum Blockchain!</p>
-              <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
-              <h2>Upload Image</h2>
+              <h1>Your sound</h1>
+              <p>This sound is stored on IPFS & The Ethereum Blockchain!</p>
+              <audio src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} controls />
+              <h2>Upload sound</h2>
               <form onSubmit={this.onSubmit} >
                 <input type='file' onChange={this.captureFile} />
                 <input type='submit' />
